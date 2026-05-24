7582b5097bbde3d069fb8225dfbba56a553cea74
Elan Hickler <elanhickler@gmail.com>
2026-05-24 01:36:54 -0700

    add basic circuit validation helper

--- include/soemdsp/runtime/debug/ValidateCircuit.hpp
+++ include/soemdsp/runtime/debug/ValidateCircuit.hpp
@@ -1,12 +1,8 @@
 #pragma once
-
 #include <iostream>
-
 #include <soemdsp/runtime/Circuit.hpp>
-
 namespace soemdsp::runtime::debug
 {
-
 inline bool validateCircuit(const Circuit& circuit)
 {
     bool valid = true;
@@ -16,38 +12,35 @@ inline bool validateCircuit(const Circuit& circuit)
         if (!connection.valid())
         {
             std::cout
-                << "[INVALID CONNECTION] "
-                << connection.id
-                << "\n";
+              << "[INVALID CONNECTION] "
+              << connection.id
+              << "\n";
 
             valid = false;
             continue;
         }
-
         if (connection.sourcePort->direction !=
             PortDirection::Output)
         {
             std::cout
-                << "[INVALID SOURCE PORT] "
-                << connection.id
-                << "\n";
+              << "[INVALID SOURCE PORT] "
+              << connection.id
+              << "\n";
 
             valid = false;
         }
-
         if (connection.destinationPort->direction !=
             PortDirection::Input)
         {
             std::cout
-                << "[INVALID DESTINATION PORT] "
-                << connection.id
-                << "\n";
+              << "[INVALID DESTINATION PORT] "
+              << connection.id
+              << "\n";
 
             valid = false;
         }
     }
-
     return valid;
 }
 
-} // namespace soemdsp::runtime::debug
+} //namespace soemdsp::runtime::debug
